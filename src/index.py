b972566efe32ef75ce2caa8d17af687d141a92e4
Juho <JuhoPaananen@users.noreply.github.com>
2023-11-06 18:51:25 +0200

    Add pylint

--- src/index.py
+++ src/index.py
@@ -1,19 +1,22 @@
 from varasto import Varasto
 
-
-def main():
+def luo_varastot():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
+    return mehua, olutta
 
+def alkutilanne(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def muokkaa_olutta(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def muokkaa_mehua(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +25,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def virhetilanteita():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,6 +35,7 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def muokkaa_varastoa_suurilla_luvuilla(mehua, olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
@@ -47,12 +52,21 @@ def main():
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def mehuvaraston_virhe(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
 
+def main():
+    mehua, olutta = luo_varastot()
+    alkutilanne(mehua, olutta)
+    muokkaa_olutta(olutta)
+    muokkaa_mehua(mehua)
+    virhetilanteita()
+    muokkaa_varastoa_suurilla_luvuilla(mehua, olutta)
+    mehuvaraston_virhe(mehua)
 
 if __name__ == "__main__":
     main()
